2b0e15cb08c9666a9c95283e1bf3a16fb8be1019
LuanAmunesc123 <estagiarioti@amunesc.org.br>
2023-05-19 13:40:56 -0300

    Iniciando pagina principal

---
+++ src/VIEW/FrmNovoProjeto.java
@@ -0,0 +1,47 @@
+package VIEW;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.TextField;
+
+public class FrmNovoProjeto extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmNovoProjeto frame = new FrmNovoProjeto();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmNovoProjeto() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 720, 444);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		TextField textField = new TextField();
+		textField.setBounds(34, 178, 310, 70);
+		contentPane.add(textField);
+	}
+
+}
