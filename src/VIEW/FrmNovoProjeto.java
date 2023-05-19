5e6b87a34f8e5288ea5bc7d7d25154f4dc0e0b7c
LuanAmunesc123 <estagiarioti@amunesc.org.br>
2023-05-19 13:53:39 -0300

    Iniciando Tabela

--- src/VIEW/FrmNovoProjeto.java
+++ src/VIEW/FrmNovoProjeto.java
@@ -1,19 +1,21 @@
 package VIEW;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.TextField;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FrmNovoProjeto extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
+	private DefaultTableModel tableModel;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -27,21 +29,43 @@ public class FrmNovoProjeto extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public FrmNovoProjeto() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 720, 444);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		TextField textField = new TextField();
-		textField.setBounds(34, 178, 310, 70);
-		contentPane.add(textField);
+
+		table = new JTable();
+		tableModel = new DefaultTableModel(
+				new Object[][] {
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+				},
+				new String[] {
+						"New column", "New column", "New column", "New column", "New column"
+				}
+		);
+		table.setModel(tableModel);
+		table.setBounds(10, 72, 694, 96);
+		contentPane.add(table);
+
+		JButton btnNewButton = new JButton("Add Project");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addProject();
+			}
+		});
+		btnNewButton.setBounds(90, 318, 165, 23);
+		contentPane.add(btnNewButton);
 	}
 
+	private void addProject() {
+		Object[] rowData = {"Project Name", "Start Date", "End Date", "Status", "Description"};
+		tableModel.addRow(rowData);
+	}
 }
